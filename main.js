083a8fe6905217383128dc14f842653b0d919e4f
SHIFU <tamangnischal2018@gmail.com>
2024-03-22 11:34:30 +0545

    video calling multiperson feature

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ let remoteUsers = {}
 
 let joinAndDisplayLocalStream = async () => {
 
+     client.on('user-published', handleUserJoined)
 
-    
     let UID = await client.join(APP_ID, CHANNEL, TOKEN, null)
     localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
     let player = `<div class="video-container" id="user-container-${UID}">
@@ -27,23 +27,26 @@ let joinStream = async () => {
 }
 
 
-let handleUserJoined = async (user , mediaType) => {
-    remoteUsers[user.UID] = user 
+let handleUserJoined = async (user, mediaType) => {
+    remoteUsers[user.uid] = user 
     await client.subscribe(user, mediaType)
+
     if (mediaType === 'video'){
         let player = document.getElementById(`user-container-${user.uid}`)
         if (player != null){
             player.remove()
         }
+
         player = `<div class="video-container" id="user-container-${user.uid}">
-        <div class="video-player" id="user-${user.uid}"></div>
-        </div>`
-        document.getElementById('video-streams').insertAdjacentElement('beforeend', player)
+                        <div class="video-player" id="user-${user.uid}"></div> 
+                 </div>`
+        document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
+
         user.videoTrack.play(`user-${user.uid}`)
     }
 
-    if(mediaType === 'audio'){
+    if (mediaType === 'audio'){
         user.audioTrack.play()
     }
- }
+}
 document.getElementById('join-btn').addEventListener('click', joinStream)
