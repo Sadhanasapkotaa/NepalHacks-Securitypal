899ce034d0bd96a6fc14904d68987d3caa6fd328
SHIFU <tamangnischal2018@gmail.com>
2024-03-22 09:57:16 +0545

    API integrating

--- main.js
+++ main.js
@@ -1,11 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+const APP_ID = "6f3b4c9135de4bc5ae86540f7025c566"
